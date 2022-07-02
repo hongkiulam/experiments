b4823ce371c4e5b2bdf12dbc0d6fd2c02234e1a0
Haydon Lam <lamhaydon@gmail.com>
2022-07-02 22:41:04 +0100

    Add playdate page

--- src/lib/pages.ts
+++ src/lib/pages.ts
@@ -8,7 +8,12 @@ const pages: Page[] = [
     title: 'Spline Logo',
     path: '001',
     description:
-      "Trying to learn how to use Spline from spline.design. I based this model on something from their community library and recreated my company's logo "
+      "Trying to learn how to use Spline from spline.design. I based this model on something from their community library and recreated my company's logo"
+  },
+  {
+    title: "Spline Playdate",
+    path: '002',
+    description: "To further to skills in Spline and non-parametric modelling - I entered a Spline competition hosted by cabeza_patata and built this as the entry."
   }
 ];
 
--- src/routes/experiments/002.svelte
+++ src/routes/experiments/002.svelte
@@ -1,29 +1,15 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-
-  let canvas: HTMLCanvasElement = null;
-
-  onMount(() => {
-    const ctx = canvas.getContext('2d');
-    ctx.fillRect(0, 0, 150, 150);
-    ctx.save();
-    ctx.fillStyle = '#09F';
-    ctx.fillRect(15, 15, 120, 120);
-    ctx.save();
-    ctx.fillStyle = 'white';
-    ctx.globalAlpha = 0.5;
-    ctx.fillRect(30, 30, 90, 90);
-    ctx.restore();
-    ctx.fillRect(45, 45, 60, 60);
-    ctx.restore();
-    ctx.fillRect(60, 60, 30, 30);
-  });
 </script>
 
-<canvas bind:this={canvas} width="400" height="400" />
+<iframe
+  src="https://my.spline.design/playdate-1f9d767b36d1e2d316f7a146fe1312d8/"
+  frameborder="0"
+  title="Spline Playdate"
+/>
 
 <style>
-  canvas {
-    border: 1px solid var(--color-primary);
+  iframe {
+    width: 100%;
+    height: 100%;
   }
 </style>
